c262204158ae40d131b5f1744ea91bfc89e692fd
ItsikEdri <itsik.edri@metropolinet.co.il>
2025-03-06 15:34:57 +0200

    move details to component folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
+import { CharacterDetailsComponent } from './components/character-details/character-details.component';
 import { EpisodesComponent } from './pages/episodes/episodes.component';
 
 export const routes: Routes = [
