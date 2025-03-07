397a44da7830f8719fd6a6f3da5ebf1a7fdd8f67
ItsikEdri <itsik.edri@metropolinet.co.il>
2025-03-07 15:24:41 +0200

    different approch to filter the characters

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,18 +1,18 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { Character } from '../models/character';
+// import { Pipe, PipeTransform } from '@angular/core';
+// import { Character } from '../models/character';
 
-@Pipe({
-  name: 'filter',
-  standalone: true
-})
-export class FilterPipe implements PipeTransform {
-  transform(characters: Character[], searchName: string, searchStatus: string): Character[] {
-    if (!characters) return [];
+// @Pipe({
+//   name: 'filter',
+//   standalone: true
+// })
+// export class FilterPipe implements PipeTransform {
+//   transform(characters: Character[], searchName: string, searchStatus: string): Character[] {
+//     if (!characters) return [];
     
-    return characters.filter(character => {
-      const nameMatch = !searchName || character.name.toLowerCase().includes(searchName.toLowerCase());
-      const statusMatch = !searchStatus || character.status.toLowerCase() === searchStatus.toLowerCase();
-      return nameMatch && statusMatch;
-    });
-  }
-}
+//     return characters.filter(character => {
+//       const nameMatch = !searchName || character.name.toLowerCase().includes(searchName.toLowerCase());
+//       const statusMatch = !searchStatus || character.status.toLowerCase() === searchStatus.toLowerCase();
+//       return nameMatch && statusMatch;
+//     });
+//   }
+// }
